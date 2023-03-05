cd0d83e9c6c3639377e02bfb2135bb347f8fffb3
JesperBodin <112554269+JesperBodin@users.noreply.github.com>
2023-03-05 12:39:48 +0100

    Add CRUD to GameResource.java
    Update Mapper.java

--- src/main/java/se/iths/laboration1/mapper/Mapper.java
+++ src/main/java/se/iths/laboration1/mapper/Mapper.java
@@ -12,4 +12,14 @@ public class Mapper {
 	public List<GameDto> map(List<Game> all){
 		return all.stream().map(GameDto::new).toList();
 	}
+	public GameDto map(Game game) {
+		return new GameDto(game);
+	}
+	public Game map(GameDto game) {
+		var g = new Game();
+		g.setId( game.getId());
+		g.setName(game.getName());
+		g.setShortening(game.getShortening());
+		return g;
+	}
 }
